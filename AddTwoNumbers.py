fc37fa5f692e2879b544aebdc6c1e3a66d09d663
ShriyaRatna <shriya.ratna7@gmail.com>
2024-10-28 21:55:40 +0530

    Solved Add Two Numbers

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -3,6 +3,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         """
@@ -33,10 +35,11 @@ class Solution(object):
             for i in range(x, len(list2)):
                 list1.append(0)
 
-        list1.reverse()
-        list2.reverse()
+        # list1.reverse()
+        # list2.reverse()
         ans = []
         carry = 0
+        print(len(list1), len(list2))
         for i in range(len(list1)):
             x = list1[i] + list2[i] + carry
             if x > 9:
@@ -44,13 +47,16 @@ class Solution(object):
             else:
                 carry = 0
             ans.append(x % 10)
+        if carry == 1:
+            ans.append(1)
 
-        # curr = ListNode()
-        # head = curr
+        # ans.reverse()
+
+        curr = ListNode(ans[0])
+        head = curr
         # curr.val = ans[0]
-
-        # for i in range(1, len(ans)):
-        #     temp = ListNode(ans[i])
-        #     curr.next = temp
-        #     curr = curr.next
-        # return head
+        for i in range(1, len(ans)):
+            temp = ListNode(ans[i])
+            curr.next = temp
+            curr = curr.next
+        return head
