427f2c6ef6a0279ede19be5966ef6346bd8a9ae8
ShriyaRatna <shriya.ratna7@gmail.com>
2024-11-17 23:54:00 +0530

    nothing important

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -9,3 +9,12 @@ class Solution(object):
             if x[i] != x[len(x) - 1 - i]:
                 return False
         return True
+
+
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         if x < 0:
+#             return False
+#         return str(x) == str(x)[::-1]
+        
+	    
