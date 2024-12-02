380d8775652b7deee2d72d1fc35f946e8a664f31
ShriyaRatna <shriya.ratna7@gmail.com>
2024-12-02 23:46:29 +0530

    Cleaned code a little

--- CountII.js
+++ CountII.js
@@ -1,7 +1,3 @@
-/**
- * @param {integer} init
- * @return { increment: Function, decrement: Function, reset: Function }
- */
 var createCounter = function(init) {
     let presentCount = init;
   
@@ -20,9 +16,3 @@ var createCounter = function(init) {
     return { increment, decrement, reset };
   };
   
-  /**
-   * const counter = createCounter(5)
-   * counter.increment(); // 6
-   * counter.reset(); // 5
-   * counter.decrement(); // 4
-   */
