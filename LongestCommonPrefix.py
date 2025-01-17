ba7bef45f6dd4e674255e3e70178b1fd1dc4221d
Shriya Ratna <94185133+ShriyaRatna@users.noreply.github.com>
2025-01-17 23:46:03 +0530

    Longest Common Prefix

--- LongestCommonPrefix.py
+++ LongestCommonPrefix.py
@@ -12,3 +12,15 @@ class Solution:
                 pref = pref[0:pref_len]
 
         return pref
+
+# class Solution:
+#     def longestCommonPrefix(self, v: List[str]) -> str:
+#         ans=""
+#         v=sorted(v)
+#         first=v[0]
+#         last=v[-1]
+#         for i in range(min(len(first),len(last))):
+#             if(first[i]!=last[i]):
+#                 return ans
+#             ans+=first[i]
+#         return ans 
