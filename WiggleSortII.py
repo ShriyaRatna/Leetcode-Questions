95f45e7386290ae0d26baf86878002b376747d55
ShriyaRatna <shriya.ratna7@gmail.com>
2025-01-21 23:47:46 +0530

    Solved Wiggle Sort 2

---
+++ WiggleSortII.py
@@ -0,0 +1,35 @@
+# import numpy as np
+# class Solution:
+#     def wiggleSort(self, nums: List[int]) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         num = sorted(nums)
+#         n = len(nums) // 2
+#         for i in range(len(nums)):
+#             if i%2 == 0:
+#                 nums[i] = min(num)
+#                 num.pop(0)
+#             else:
+#                 nums[i] = max(num)
+#                 num.pop()
+
+
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
+
+        n = len(nums)
+        mid = (n + 1) // 2
+
+        left = nums[:mid][::-1]
+        right = nums[mid:][::-1]
+
+        for i in range(n):
+            if i % 2 == 0:
+                nums[i] = left.pop(0)
+            else:
+                nums[i] = right.pop(0)
