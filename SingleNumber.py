b71b0a27214a4b261d9e328c5a9979cf3df78bd0
ShriyaRatna <shriya.ratna7@gmail.com>
2025-01-22 23:56:49 +0530

    New appraoch to Single Number

--- SingleNumber.py
+++ SingleNumber.py
@@ -1,11 +1,19 @@
-class Solution(object):
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        set1 = set(nums)
-        for i in set1:
-            if nums.count(i) == 1:
-                return i
-        return None
+# class Solution(object):
+#     def singleNumber(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         set1 = set(nums)
+#         for i in set1:
+#             if nums.count(i) == 1:
+#                 return i
+#         return None
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
